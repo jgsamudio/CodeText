7173969b6b95e88c98719ca1b0667730f4464ac4
jgsamudio <jonathan2457@gmail.com>
2019-04-24 21:11:01 -0400

    Fix performance issues with pasting text into the text editor

--- CodeText/CodeTextView/CodeTextView.swift
+++ CodeText/CodeTextView/CodeTextView.swift
@@ -28,27 +28,16 @@ class CodeTextView: NSTextView {
     
     
     override func didChangeText() {
-        textStorage?.words.forEach { print($0) }
-        
-        updateText()
+//        textStorage?.words.forEach { print($0) }
+//        updateText()
     }
 }
 
 private extension CodeTextView {
     
     func initialize() {
-        delegate = self
+//        delegate = self
         textStorage?.delegate = self
-    }
-    
-    func updateText() {
-//        setTextColor(NSColor.red, range: NSRange(location: 0, length: 1))
-//        textStorage?.words.forEach {
-////            print($0)
-//            if $0.delegate == 'func' {
-//                setTextColor(NSColor.red, range: NSRange(location: 0, length: 1))
-//            }
-//        }
     }
 }
 
@@ -61,12 +50,6 @@ extension CodeTextView: NSTextStorageDelegate {
                      range editedRange: NSRange,
                      changeInLength delta: Int) {
         
-//        textStorage.words.forEach {
-//            if $0.string == "func" {
-//                $0.addAttribute(NSAttributedString.Key.foregroundColor, value: NSColor.red, range: NSMakeRange(0, $0.length))
-//            }
-//        }
-        
         var start = editedRange.lowerBound + min(delta, 0)
         if start < 0 {
             return
@@ -76,33 +59,47 @@ extension CodeTextView: NSTextStorageDelegate {
             start -= 1
         }
         
-        let length = (editedRange.lowerBound - start) + editedRange.length
-        let updatedRange = NSMakeRange(start, (length))
+        var end = editedRange.upperBound
         
-//        let attrString = textStorage.attributedSubstring(from: updatedRange).string
+        while end < textStorage.characters.count, textStorage.characters[end].string != " " {
+            end += 1
+        }
+        
+        let length = (editedRange.lowerBound - start) + editedRange.length + (end - editedRange.upperBound)
+        let updatedRange = NSMakeRange(start, (length))
         
         var tempString = ""
         var range = NSMakeRange(updatedRange.location, 0)
-        print(range)
+        
+        // Preload the character array
+        // Needed for performance.
+        let textCharacters = textStorage.characters
         
         for i in updatedRange.location..<(updatedRange.location + updatedRange.length) {
-            let currentCharacter = textStorage.characters[i].string
-            print(currentCharacter)
-            
+            let currentCharacter = textCharacters[i].string
             if currentCharacter == " " {
-                range = NSMakeRange(range.location + tempString.count, 0)
+                // Reset the range
+                // Space of the space +1
+                range.location += tempString.count + 1
+                range.length = 0
                 tempString = ""
             } else {
                 tempString += currentCharacter
-                range = NSMakeRange(range.location, range.length + 1)
-                
+                range.length += 1
+
                 if tempString == "func" ||
                     tempString == "extension" ||
                     tempString == "var" ||
                     tempString == "if" ||
                     tempString == "else" {
                     textStorage.addAttribute(NSAttributedString.Key.foregroundColor, value: NSColor.red, range: range)
+
+                    // Reset the range
+                    range.location += tempString.count
+                    range.length = 0
+                    tempString = ""
                 } else {
+                    print(range)
                     textStorage.addAttribute(NSAttributedString.Key.foregroundColor, value: NSColor.white, range: range)
                 }
             }
@@ -110,26 +107,26 @@ extension CodeTextView: NSTextStorageDelegate {
     }
 }
 
-extension CodeTextView: NSTextViewDelegate {
-    
-    // Delegate only.  If characters are changing, replacementString is what will replace the affectedCharRange.
-    // If attributes only are changing, replacementString will be nil.  Will not be called if
-    // textView:shouldChangeTextInRanges:replacementStrings: is implemented.
-    func textView(_ textView: NSTextView,
-                  shouldChangeTextIn affectedCharRange: NSRange,
-                  replacementString: String?) -> Bool {
-//        print(replacementString!)
-        return true
-    }
-    
+//extension CodeTextView: NSTextViewDelegate {
+//
+//    // Delegate only.  If characters are changing, replacementString is what will replace the affectedCharRange.
+//    // If attributes only are changing, replacementString will be nil.  Will not be called if
+//    // textView:shouldChangeTextInRanges:replacementStrings: is implemented.
 //    func textView(_ textView: NSTextView,
-//                  shouldChangeTypingAttributes oldTypingAttributes: [String : Any] = [:],
-//                  toAttributes newTypingAttributes: [NSAttributedString.Key : Any] = [:]) -> [NSAttributedString.Key : Any] {
-//        print(oldTypingAttributes)
-//        print(newTypingAttributes)
-//        return newTypingAttributes
+//                  shouldChangeTextIn affectedCharRange: NSRange,
+//                  replacementString: String?) -> Bool {
+////        print(replacementString!)
+//        return true
 //    }
-}
+//
+////    func textView(_ textView: NSTextView,
+////                  shouldChangeTypingAttributes oldTypingAttributes: [String : Any] = [:],
+////                  toAttributes newTypingAttributes: [NSAttributedString.Key : Any] = [:]) -> [NSAttributedString.Key : Any] {
+////        print(oldTypingAttributes)
+////        print(newTypingAttributes)
+////        return newTypingAttributes
+////    }
+//}
 
 class CodeHighlighter {
     
